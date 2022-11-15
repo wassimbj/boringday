40e2b620420a79233f7a3774ba78195d542ba810
wassim <wassimbenjdida@gmail.com>
2022-11-15 09:17:12 +0100

    finished the week calendar

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -23,32 +23,30 @@ export class HomeComponent implements OnInit {
   currentMonth = new Date();
   currentWeek = getWeek(this.currentMonth);
 
-  rows: Array<any> = [];
+  weekCells: Array<any> = [];
   selectedDate = new Date();
   renderWeekCells() {
     let startDate = startOfWeek(this.currentMonth, { weekStartsOn: 1 });
-    let endDate = lastDayOfWeek(this.currentMonth, { weekStartsOn: 1 });
     let days: Array<any> = [];
     let day = startDate;
-    while (day <= endDate) {
-      for (let i = 0; i < 7; i++) {
-        // const cloneDay = day;
-        days.push({
-          dayDate: format(day, 'd'),
-          dayName: format(day, 'eee'), // Mon - Sun
-          isToday: isSameDay(day, new Date()),
-          isSelectedDate: isSameDay(day, this.selectedDate),
-          day,
-        });
-        day = addDays(day, 1);
-      }
-      this.rows = days;
-      days = [];
+    for (let i = 0; i < 7; i++) {
+      days.push({
+        dayDate: format(day, 'd'),
+        dayName: format(day, 'eee'), // Mon - Sun
+        isToday: isSameDay(day, new Date()),
+        isSelectedDate: isSameDay(day, this.selectedDate),
+        day,
+      });
+      day = addDays(day, 1);
     }
+    this.weekCells = days;
+    days = [];
   }
 
   handleSelectDate(date: Date) {
     console.log(date);
+    this.selectedDate = date;
+    this.renderWeekCells()
   }
 
   handleChangeWeek(btnType: 'prev' | 'next') {
@@ -59,6 +57,16 @@ export class HomeComponent implements OnInit {
     if (btnType === 'next') {
       this.currentMonth = addWeeks(this.currentMonth, 1);
       this.currentWeek = getWeek(addWeeks(this.currentMonth, 1));
+    }
+    this.renderWeekCells();
+  }
+
+  handleChangeMonth(btnType: 'prev' | 'next') {
+    if (btnType === 'prev') {
+      this.currentMonth = subMonths(this.currentMonth, 1);
+    }
+    if (btnType === 'next') {
+      this.currentMonth = addMonths(this.currentMonth, 1);
     }
     this.renderWeekCells();
   }
